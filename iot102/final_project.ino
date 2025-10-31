e53682460226a464a487ee9337f12e4b678b7b71
ductohno <152991010+ductohno@users.noreply.github.com>
2025-10-31 10:40:45 +0700

    Update final_project.ino

--- iot102/final_project.ino
+++ iot102/final_project.ino
@@ -1,15 +1,18 @@
-#define BLYNK_TEMPLATE_ID "xxxxx"
-#define BLYNK_AUTH_TOKEN "xxxxx"
-#include <BlynkSimpleEsp32.h>   
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME "Watering automatic"
+#define BLYNK_AUTH_TOKEN ""
+#include <BlynkSimpleEsp32.h> 
+#include <WiFi.h>  
+#include <WiFiClient.h>
+#include "DHT.h"
 
-char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "YourWiFi";
 char pass[] = "YourPass";
 
 // port of sensor and 5v relay
 int moistSensor = 34;
-int tempSensor = 35;
-int relay = 9;
+int tempSensor = 33;
+int relay = 25;
 
 // constant
 int minMoist = 30;
@@ -17,6 +20,7 @@ int maxMoist = 50;
 int minTemp = 15;
 int readSensorDelayPumpOn = 500;
 int readSensorDelayPumpOff = 2000;
+int manualDelay = 100;
 int displayDelay = 1000;
 
 // status
@@ -24,9 +28,13 @@ int pumpOpen = 0;
 int currentMoist = 0;
 int currentTemp = 0;
 unsigned long delayOfSensor = 0;
-unsigned long tDisplay=0, tSensor=0 ,t;
+unsigned long tDisplay=0, tSensor=0 ,tManual=0, t;
 bool isAutoMode = false;
 bool manualInput = false;
+
+// DHT config
+#define DHTTYPE DHT11    
+DHT dht(tempSensor, DHTTYPE);
 
 // read blynk button
 BLYNK_WRITE(V0) { 
@@ -41,7 +49,12 @@ BLYNK_WRITE(V1) {
 void sendToBlynk() {
   Blynk.virtualWrite(V2, currentMoist);
   Blynk.virtualWrite(V3, currentTemp);
-  Blynk.virtualWrite(V4, pumpOpen);
+  if(pumpOpen){
+    Blynk.virtualWrite(V4, "ON");
+  }
+  else{
+    Blynk.virtualWrite(V4, "OFF");
+  }
 }
 
 void setup() {
@@ -50,22 +63,29 @@ void setup() {
   pinMode(tempSensor, INPUT);
   pinMode(relay, OUTPUT);
   digitalWrite(relay, pumpOpen);
-  Blynk.begin(auth, ssid, pass);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 void loop() {
   Blynk.run();
   // Receive data from sensor
-  currentMoist = map(analogRead(moistSensor), 0, 1023, 0, 100);
-  currentTemp = map(analogRead(tempSensor), 0, 1023, 0, 50);
+  currentMoist = map(analogRead(moistSensor), 0, 4095, 0, 100);
+  currentTemp = dht.readTemperature();
+
+  if (isnan(currentTemp)) {
+    Blynk.virtualWrite(V4, "Temp sensor eror");
+    return;
+  }
+
   // Receive status
   t = millis();
   if(t-tDisplay >= displayDelay){
     sendToBlynk();
-    Serial.print("Moist: "); Serial.print(currentMoist);
-    Serial.print("% | Temp: "); Serial.print(currentTemp);
-    Serial.print("°C | Pump: "); Serial.println(pumpOpen);
     tDisplay=t;
+    if (isnan(currentTemp)) {
+      Blynk.virtualWrite(V4, "Temp sensor eror");
+      return;
+    }
   }
   // Auto mode or manual mode
   if(isAutoMode == true){
@@ -78,7 +98,7 @@ void loop() {
     }
     if( t-tSensor >= delayOfSensor ){
       if( currentMoist <= minMoist || pumpOpen == 1){
-        if( currentTemp >= minTemp ){
+        if( currentTemp > minTemp ){
           // If plant not enough moist, open pump
           if( currentMoist <= maxMoist ){
             pumpOpen=1;
@@ -102,14 +122,17 @@ void loop() {
     }
   }
   else{
-    // open pump if manualInput is true
-    if( manualInput == true){
-      pumpOpen=1;
+    if(t-tManual > manualDelay){
+      // open pump if manualInput is true
+      if( manualInput == true){
+        pumpOpen=1;
+      }
+      // close pump if manualInput is false
+      else{
+        pumpOpen=0;
+      }
+      digitalWrite(relay, pumpOpen);
+      tManual = t;
     }
-    // close pump if manualInput is false
-    else{
-      pumpOpen=0;
-    }
-    digitalWrite(relay, pumpOpen);
   }
 }
